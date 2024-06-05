2e7716be25c6374a80c6e6c477ad234482c14e29
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-06-05 17:37:47 +0300

    switch to wikimapia.org maps

--- map.js
+++ map.js
@@ -4,6 +4,7 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import { fromLonLat } from 'ol/proj';
 import { XYZ } from 'ol/source';
+import { createXYZ } from 'ol/tilegrid';
 
 const MAP_TARGET = 'map';
 const MAP_CENTER = fromLonLat([37.57725139554275, 48.02287702854201]);
@@ -11,7 +12,13 @@ const MAP_ZOOM = 8.5;
 
 const customTileSource = new TileLayer({
     source: new XYZ({
-        url: 'https://tile-server.ozon.ru/tile/default/{z}/{x}/{y}.png'
+        // url: 'https://tile-server.ozon.ru/tile/default/{z}/{x}/{y}.png'
+
+        tileUrlFunction: ([z, x, y]) => {
+            const s = x % 4 + (y % 4) * 4
+            return `https://i${s}.wikimapia.org/?x=${x}&y=${y}&zoom=${z}&type=map&lng=1`
+        },
+        maxZoom: 18,
     })
 });
 
