baa73c0c2eafccac336517f10e13d80f1766b92e
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-06-09 08:14:56 +0300

    fix: hide map while popup is opened

--- src/info-popup.js
+++ src/info-popup.js
@@ -1,22 +1,26 @@
 import Cookies from "js-cookie";
+import { el } from "./utils.js";
 
 const HIDE_INFO_POPUP = "hideInfoPopup";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const infoPopup = document.querySelector(".info-popup");
-    const noShowCheckbox = document.querySelector('input[name="no-show"]');
-    const confirmButton = document.querySelector('.info-popup footer button');
+  const infoPopup = document.querySelector(".info-popup");
+  const noShowCheckbox = document.querySelector('input[name="no-show"]');
+  const confirmButton = document.querySelector(".info-popup footer button");
+  const map = el("map");
 
-    const hidePopupCookie = Cookies.get(HIDE_INFO_POPUP);
-    if (!hidePopupCookie) {
-        infoPopup.style.display = "block";
-      }
+  const hidePopupCookie = Cookies.get(HIDE_INFO_POPUP);
+  if (!hidePopupCookie) {
+    infoPopup.style.display = "block";
+    map.style.display = "none";
+  }
 
-    confirmButton.addEventListener("click", function () {
-        if (noShowCheckbox.checked) {
-          Cookies.set(HIDE_INFO_POPUP, "true", { expires: 400 });
-        }
+  confirmButton.addEventListener("click", function () {
+    if (noShowCheckbox.checked) {
+      Cookies.set(HIDE_INFO_POPUP, "true", { expires: 400 });
+    }
 
-        infoPopup.style.display = "none";
-      });
+    infoPopup.style.display = "none";
+    map.style.display = "block";
+  });
 });
